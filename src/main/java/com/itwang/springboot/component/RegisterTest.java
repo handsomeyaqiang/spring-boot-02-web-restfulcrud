4a53417e1a080cb63b1aa91f4b14bb4c900d8fa3
王亚强 <2259196468@qq.com>
2018-05-25 16:56:30 +0800

    更新文件信息

--- src/main/java/com/itwang/springboot/component/RegisterTest.java
+++ src/main/java/com/itwang/springboot/component/RegisterTest.java
@@ -3,5 +3,6 @@ package com.itwang.springboot.component;
 public class RegisterTest {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("this is second test");
     }
 }
